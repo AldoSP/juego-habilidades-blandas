46031195f4fb7948f8652636064463bb64e44ecd
EdwinGut <edwing.gutierrez@udea.edu.co>
2026-04-28 22:14:44 -0500

    imagen de título
    Cambios de imagen de título

--- escenas/escena_2.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		cambiar_escena()
-
-func cambiar_escena():
-	get_tree().change_scene_to_file("res://escenas/escena_trabajo.tscn")
--- scripts/escena.gd
+++ scripts/escena.gd
@@ -1,31 +1,7 @@
 extends Node2D
 
-var dialogos = [
-	{"nombre": "NombrePersonaje", "texto": "Hola... ¿estás ahí?"},
-	{"nombre": "NombrePersonaje", "texto": "Este es nuestro primer diálogo."},
-	{"nombre": "NombrePersonaje", "texto": "Vamos a cambiar de escena pronto..."}
-]
 
-var indice = 0
 
-@onready var texto = $UI/CajaDialogo/Texto
-@onready var nombre = $UI/CajaDialogo/Nombre
 
-func _ready():
-	mostrar_dialogo()
-
-func mostrar_dialogo():
-	nombre.text = dialogos[indice]["nombre"]
-	texto.text = dialogos[indice]["texto"]
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		indice += 1
-		
-		if indice < dialogos.size():
-			mostrar_dialogo()
-		else:
-			cambiar_escena()
-
-func cambiar_escena():
-	get_tree().change_scene_to_file("res://escenas/escena_2.tscn")
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://escenas/main_game.tscn")
