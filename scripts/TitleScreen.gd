0b1da321b66723f9acdce31aa9cea75eb976afce
AldoSP <99729660+AldoSP@users.noreply.github.com>
2026-04-29 23:29:37 -0500

    Intro cutscene implemented
    The intro cutscene has been implemented. Pressing the play button will start the dialogic timeline containing the introduction. After it's done playing it takes you to the main game screen

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -1,7 +1,31 @@
 extends Control
 
+const INTRO_TIMELINE_ID := "Introduction"
+const MAIN_GAME_SCENE := "res://escenas/main_game.tscn"
 
+var _intro_started := false
+
+
+func _ready() -> void:
+	if not Dialogic.timeline_ended.is_connected(_on_dialogic_timeline_ended):
+		Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
+
+
+func _exit_tree() -> void:
+	if Dialogic.timeline_ended.is_connected(_on_dialogic_timeline_ended):
+		Dialogic.timeline_ended.disconnect(_on_dialogic_timeline_ended)
 
 
 func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://escenas/main_game.tscn")
+	if _intro_started:
+		return
+
+	_intro_started = true
+	Dialogic.start(INTRO_TIMELINE_ID)
+
+
+func _on_dialogic_timeline_ended() -> void:
+	if not _intro_started:
+		return
+
+	get_tree().change_scene_to_file(MAIN_GAME_SCENE)
